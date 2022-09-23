ffeeca1bcf621ea5c07d8fd747720f1cb87bb2f7
samuelvalverdee <samuel.valverde.2802@gmail.com>
2022-09-22 20:09:31 -0600

    espacios
    nada

--- quiz#6/src/quiz6/MainQuiz.java
+++ quiz#6/src/quiz6/MainQuiz.java
@@ -6,9 +6,9 @@ import java.util.Collections;
 public class MainQuiz {
 
 	public static void main(String[] args) {
-		
+
 		ArrayList<Participante> participantes = new ArrayList<Participante>();
-		
+
 		participantes.add(new Participante("Jimena",(int)(Math.random()*5)+7,(int)(Math.random()*10)+7));
 		participantes.add(new Participante("Isaac",(int)(Math.random()*5)+7,(int)(Math.random()*10)+7));
 		participantes.add(new Participante("Jose Daniel",(int)(Math.random()*5)+7,(int)(Math.random()*10)+7));
@@ -16,12 +16,12 @@ public class MainQuiz {
 		participantes.add(new Participante("Santiago",(int)(Math.random()*5)+7,(int)(Math.random()*10)+7));
 		participantes.add(new Participante("Nicole",(int)(Math.random()*5)+7,(int)(Math.random()*10)+7));
 		participantes.add(new Participante("Samuel",(int)(Math.random()*5)+7,(int)(Math.random()*10)+7));
-		
+
 		Collections.sort(participantes);
-		
+
 		System.out.println("Nombre          Juegos     Puntos");
 		System.out.println("---------------------------------");
-		
+
 		for(Participante pt : participantes) {
 			System.out.println(pt.getNombre()+"        "+pt.getJuegos()+"   -   "+pt.getPuntos());
 		}
